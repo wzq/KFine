e4a4ec00599ab83be8ea0d801758488f6a2595be
wzq <297001985@qq.com>
2015-12-21 11:29:20 +0800

    init

---
+++ app/src/main/java/com/firstlink/duo/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.firstlink.duo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by wzq on 15/12/21.
+ */
+class BaseActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
